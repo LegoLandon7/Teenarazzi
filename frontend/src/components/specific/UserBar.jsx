997d9715bd5541e2d65ff5e8f514887afefcdf5a
Landon Lego <legolandon25@gmail.com>
2026-03-01 00:14:28 +0000

    added some users

--- frontend/src/components/specific/UserBar.jsx
+++ frontend/src/components/specific/UserBar.jsx
@@ -7,6 +7,15 @@ function UserBar() {
 
             <div className="user-links">
                 <NavLink to="legomaster_01">legomaster_01</NavLink>
+                <NavLink to="mracticalpacaron">mracticalpacaron</NavLink>
+                <NavLink to="notagod_420">notagod_420</NavLink>
+                <NavLink to="ejoit">ejoit</NavLink>
+                <NavLink to="thesillyman">thesillyman</NavLink>
+                <NavLink to="notagod_420">notagod_420</NavLink>
+                <NavLink to="mars_rover_47704">mars_rover_47704</NavLink>
+                <NavLink to="therealyuyuu">therealyuyuu</NavLink>
+                <NavLink to="wyld_thais.">wyld_thais.</NavLink>
+                <NavLink to="vivi.the.smiling">vivi.the.smiling</NavLink>
             </div>
         </nav>
     )
